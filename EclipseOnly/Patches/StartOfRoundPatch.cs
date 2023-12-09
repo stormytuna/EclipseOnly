0d14a4b1c2d1edf4bd2cae1ada4282f93ae492ba
stormytuna <stormytuna@outlook.com>
2023-12-09 10:43:40 +0000

    Add project files.

---
+++ EclipseOnly/Patches/StartOfRoundPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace EclipseOnly.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    public class StartOfRoundPatch
+    {
+        [HarmonyPrefix, HarmonyPatch(nameof(StartOfRound.SetPlanetsWeather))]
+        public static bool MakePlanetsEclipsed(SelectableLevel[] ___levels) {
+            foreach (SelectableLevel level in ___levels) {
+                level.currentWeather = LevelWeatherType.Eclipsed;
+            }
+
+            return false; // Want to completely replace functionality of this method, so no need to run the original
+        }
+    }
+}
---
+++ EclipseOnly/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace EclipseOnly
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class EclipseOnlyBase : BaseUnityPlugin
+    {
+        public const string ModGUID = "stormytuna.EclipseOnly";
+        public const string ModName = "EclipseOnly";
+        public const string ModVersion = "1.0.0";
+
+        public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+        public static EclipseOnlyBase Instance;
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        private void Awake() {
+            if (Instance is null) {
+                Instance = this;
+            }
+
+            Log.LogInfo("Eclipse Only has awoken!");
+
+            harmony.PatchAll();
+        }
+    }
+}
